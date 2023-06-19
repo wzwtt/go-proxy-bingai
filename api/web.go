8c6bd2d12eda37ad6788159f5fc2716733ab3f3a
George Wang <wzwttol@outlook.com>
2023-06-19 12:57:28 +0800

    Update web.go

--- api/web.go
+++ api/web.go
@@ -1,9 +1,9 @@
 package api
 
 import (
-	"adams549659584/go-proxy-bingai/api/helper"
-	"adams549659584/go-proxy-bingai/common"
-	"adams549659584/go-proxy-bingai/web"
+	"wzwtt/go-proxy-bingai/api/helper"
+	"wzwtt/go-proxy-bingai/common"
+	"wzwtt/go-proxy-bingai/web"
 	"net/http"
 )
 
